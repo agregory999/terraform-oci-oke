c48fb2b9ef5a01c4ccc8a8187e8c75c4c6752ed6
Andrew <andrew.gregory@oracle.com>
2022-01-04 21:50:27 -0500

    leave in modules that are needed and assume state.

--- main.tf
+++ main.tf
@@ -1,198 +1,198 @@
 # Copyright 2017, 2021 Oracle Corporation and/or affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
 
-# module "vcn" {
-#   source  = "oracle-terraform-modules/vcn/oci"
-#   version = "3.0.0"
+module "vcn" {
+  source  = "oracle-terraform-modules/vcn/oci"
+  version = "3.0.0"
 
-#   # general oci parameters
-#   compartment_id = var.compartment_id
-#   label_prefix   = var.label_prefix
+  # general oci parameters
+  compartment_id = var.compartment_id
+  label_prefix   = var.label_prefix
 
-#   # gateways
-#   create_internet_gateway  = true
-#   create_nat_gateway       = var.worker_type == "private" || var.create_operator == true || (var.load_balancers == "internal" || var.load_balancers == "both") ? true : false
-#   create_service_gateway   = true
-#   nat_gateway_public_ip_id = var.nat_gateway_public_ip_id
+  # gateways
+  create_internet_gateway  = true
+  create_nat_gateway       = var.worker_type == "private" || var.create_operator == true || (var.load_balancers == "internal" || var.load_balancers == "both") ? true : false
+  create_service_gateway   = true
+  nat_gateway_public_ip_id = var.nat_gateway_public_ip_id
 
-#   # drg
-#   create_drg       = var.create_drg
-#   drg_display_name = var.drg_display_name
+  # drg
+  create_drg       = var.create_drg
+  drg_display_name = var.drg_display_name
 
-#   # lpgs
-#   local_peering_gateways = var.local_peering_gateways
+  # lpgs
+  local_peering_gateways = var.local_peering_gateways
 
-#   # freeform_tags
-#   freeform_tags = var.freeform_tags["vcn"]
+  # freeform_tags
+  freeform_tags = var.freeform_tags["vcn"]
 
-#   # vcn
-#   vcn_cidrs                    = var.vcn_cidrs
-#   vcn_dns_label                = var.vcn_dns_label
-#   vcn_name                     = var.vcn_name
-#   lockdown_default_seclist     = var.lockdown_default_seclist
-#   internet_gateway_route_rules = var.internet_gateway_route_rules
-#   nat_gateway_route_rules      = var.nat_gateway_route_rules
+  # vcn
+  vcn_cidrs                    = var.vcn_cidrs
+  vcn_dns_label                = var.vcn_dns_label
+  vcn_name                     = var.vcn_name
+  lockdown_default_seclist     = var.lockdown_default_seclist
+  internet_gateway_route_rules = var.internet_gateway_route_rules
+  nat_gateway_route_rules      = var.nat_gateway_route_rules
 
-# }
+}
 
-# module "bastion" {
-#   source  = "oracle-terraform-modules/bastion/oci"
-#   version = "3.0.0"
+module "bastion" {
+  source  = "oracle-terraform-modules/bastion/oci"
+  version = "3.0.0"
 
-#   tenancy_id     = var.tenancy_id
-#   compartment_id = var.compartment_id
+  tenancy_id     = var.tenancy_id
+  compartment_id = var.compartment_id
 
-#   label_prefix = var.label_prefix
+  label_prefix = var.label_prefix
 
-#   # networking
-#   availability_domain = var.availability_domains["bastion"]
-#   bastion_access      = var.bastion_access
-#   ig_route_id         = module.vcn.ig_route_id
-#   netnum              = lookup(var.subnets["bastion"], "netnum")
-#   newbits             = lookup(var.subnets["bastion"], "newbits")
-#   vcn_id              = module.vcn.vcn_id
+  # networking
+  availability_domain = var.availability_domains["bastion"]
+  bastion_access      = var.bastion_access
+  ig_route_id         = module.vcn.ig_route_id
+  netnum              = lookup(var.subnets["bastion"], "netnum")
+  newbits             = lookup(var.subnets["bastion"], "newbits")
+  vcn_id              = module.vcn.vcn_id
 
-#   # bastion host parameters
-#   bastion_image_id   = var.bastion_image_id
-#   bastion_os_version = var.bastion_os_version
-#   bastion_shape      = var.bastion_shape
-#   bastion_state      = var.bastion_state
-#   bastion_timezone   = var.bastion_timezone
-#   bastion_type       = var.bastion_type
+  # bastion host parameters
+  bastion_image_id   = var.bastion_image_id
+  bastion_os_version = var.bastion_os_version
+  bastion_shape      = var.bastion_shape
+  bastion_state      = var.bastion_state
+  bastion_timezone   = var.bastion_timezone
+  bastion_type       = var.bastion_type
 
-#   ssh_public_key      = var.ssh_public_key
-#   ssh_public_key_path = var.ssh_public_key_path
-#   upgrade_bastion     = var.upgrade_bastion
+  ssh_public_key      = var.ssh_public_key
+  ssh_public_key_path = var.ssh_public_key_path
+  upgrade_bastion     = var.upgrade_bastion
 
-#   # bastion notification
-#   enable_bastion_notification   = var.enable_bastion_notification
-#   bastion_notification_endpoint = var.bastion_notification_endpoint
-#   bastion_notification_protocol = var.bastion_notification_protocol
-#   bastion_notification_topic    = var.bastion_notification_topic
+  # bastion notification
+  enable_bastion_notification   = var.enable_bastion_notification
+  bastion_notification_endpoint = var.bastion_notification_endpoint
+  bastion_notification_protocol = var.bastion_notification_protocol
+  bastion_notification_topic    = var.bastion_notification_topic
 
-#   freeform_tags = var.freeform_tags["bastion"]
+  freeform_tags = var.freeform_tags["bastion"]
 
-#   providers = {
-#     oci.home = oci.home
-#   }
+  providers = {
+    oci.home = oci.home
+  }
 
-#   depends_on = [
-#     module.vcn
-#   ]
+  depends_on = [
+    module.vcn
+  ]
 
-#   count = var.create_bastion_host == true ? 1 : 0
-# }
+  count = var.create_bastion_host == true ? 1 : 0
+}
 
-# module "operator" {
-#   source  = "oracle-terraform-modules/operator/oci"
-#   version = "3.0.0"
+module "operator" {
+  source  = "oracle-terraform-modules/operator/oci"
+  version = "3.0.0"
 
-#   tenancy_id = var.tenancy_id
+  tenancy_id = var.tenancy_id
 
-#   # general oci parameters
-#   compartment_id = var.compartment_id
-#   label_prefix   = var.label_prefix
+  # general oci parameters
+  compartment_id = var.compartment_id
+  label_prefix   = var.label_prefix
 
-#   # networking
-#   availability_domain = var.availability_domains["operator"]
-#   nat_route_id        = module.vcn.nat_route_id
-#   netnum              = lookup(var.subnets["operator"], "netnum")
-#   newbits             = lookup(var.subnets["operator"], "newbits")
-#   nsg_ids             = var.operator_nsg_ids
-#   vcn_id              = module.vcn.vcn_id
+  # networking
+  availability_domain = var.availability_domains["operator"]
+  nat_route_id        = module.vcn.nat_route_id
+  netnum              = lookup(var.subnets["operator"], "netnum")
+  newbits             = lookup(var.subnets["operator"], "newbits")
+  nsg_ids             = var.operator_nsg_ids
+  vcn_id              = module.vcn.vcn_id
 
-#   # operator host parameters
-#   operator_image_id                  = var.operator_image_id
-#   enable_operator_instance_principal = var.enable_operator_instance_principal
-#   operator_os_version                = var.operator_os_version
-#   operator_shape                     = var.operator_shape
-#   operator_state                     = var.operator_state
-#   operator_timezone                  = var.operator_timezone
-#   ssh_public_key                     = var.ssh_public_key
-#   ssh_public_key_path                = var.ssh_public_key_path
-#   upgrade_operator                   = var.upgrade_operator
+  # operator host parameters
+  operator_image_id                  = var.operator_image_id
+  enable_operator_instance_principal = var.enable_operator_instance_principal
+  operator_os_version                = var.operator_os_version
+  operator_shape                     = var.operator_shape
+  operator_state                     = var.operator_state
+  operator_timezone                  = var.operator_timezone
+  ssh_public_key                     = var.ssh_public_key
+  ssh_public_key_path                = var.ssh_public_key_path
+  upgrade_operator                   = var.upgrade_operator
 
-#   # operator notification
-#   enable_operator_notification   = var.enable_operator_notification
-#   operator_notification_endpoint = var.operator_notification_endpoint
-#   operator_notification_protocol = var.operator_notification_protocol
-#   operator_notification_topic    = var.operator_notification_topic
+  # operator notification
+  enable_operator_notification   = var.enable_operator_notification
+  operator_notification_endpoint = var.operator_notification_endpoint
+  operator_notification_protocol = var.operator_notification_protocol
+  operator_notification_topic    = var.operator_notification_topic
 
-#   freeform_tags = var.freeform_tags["operator"]
+  freeform_tags = var.freeform_tags["operator"]
 
-#   providers = {
-#     oci.home = oci.home
-#   }
+  providers = {
+    oci.home = oci.home
+  }
 
-#   depends_on = [
-#     module.vcn
-#   ]
+  depends_on = [
+    module.vcn
+  ]
 
-#   count = var.create_operator == true ? 1 : 0
-# }
+  count = var.create_operator == true ? 1 : 0
+}
 
-# module "bastionsvc" {
-#   source = "./modules/bastionsvc"
+module "bastionsvc" {
+  source = "./modules/bastionsvc"
 
-#   # general oci parameters
-#   compartment_id = var.compartment_id
-#   label_prefix   = var.label_prefix
+  # general oci parameters
+  compartment_id = var.compartment_id
+  label_prefix   = var.label_prefix
 
-#   # bastion service parameters
-#   bastion_service_access        = var.bastion_service_access
-#   bastion_service_name          = var.bastion_service_name
-#   bastion_service_target_subnet = var.bastion_service_target_subnet
-#   vcn_id                        = module.vcn.vcn_id
+  # bastion service parameters
+  bastion_service_access        = var.bastion_service_access
+  bastion_service_name          = var.bastion_service_name
+  bastion_service_target_subnet = var.bastion_service_target_subnet
+  vcn_id                        = module.vcn.vcn_id
 
-#   depends_on = [
-#     module.operator
-#   ]
+  depends_on = [
+    module.operator
+  ]
 
-#   count = var.create_bastion_service == true ? 1 : 0
-# }
+  count = var.create_bastion_service == true ? 1 : 0
+}
 
-# # additional networking for oke
-# module "network" {
-#   source = "./modules/network"
+# additional networking for oke
+module "network" {
+  source = "./modules/network"
 
-#   # general oci parameters
-#   compartment_id = var.compartment_id
-#   label_prefix   = var.label_prefix
+  # general oci parameters
+  compartment_id = var.compartment_id
+  label_prefix   = var.label_prefix
 
-#   # oke networking parameters
-#   ig_route_id  = module.vcn.ig_route_id
-#   nat_route_id = module.vcn.nat_route_id
-#   subnets      = var.subnets
-#   vcn_id       = module.vcn.vcn_id
+  # oke networking parameters
+  ig_route_id  = module.vcn.ig_route_id
+  nat_route_id = module.vcn.nat_route_id
+  subnets      = var.subnets
+  vcn_id       = module.vcn.vcn_id
 
-#   # control plane endpoint parameters
-#   control_plane_type          = var.control_plane_type
-#   control_plane_allowed_cidrs = var.control_plane_allowed_cidrs
+  # control plane endpoint parameters
+  control_plane_type          = var.control_plane_type
+  control_plane_allowed_cidrs = var.control_plane_allowed_cidrs
 
-#   # oke worker network parameters
-#   allow_node_port_access       = var.allow_node_port_access
-#   allow_worker_internet_access = var.allow_worker_internet_access
-#   allow_worker_ssh_access      = var.allow_worker_ssh_access
-#   worker_type                  = var.worker_type
+  # oke worker network parameters
+  allow_node_port_access       = var.allow_node_port_access
+  allow_worker_internet_access = var.allow_worker_internet_access
+  allow_worker_ssh_access      = var.allow_worker_ssh_access
+  worker_type                  = var.worker_type
 
-#   # oke load balancer network parameters
-#   load_balancers = var.load_balancers
+  # oke load balancer network parameters
+  load_balancers = var.load_balancers
 
-#   # oke internal load balancer
-#   internal_lb_allowed_cidrs = var.internal_lb_allowed_cidrs
-#   internal_lb_allowed_ports = var.internal_lb_allowed_ports
+  # oke internal load balancer
+  internal_lb_allowed_cidrs = var.internal_lb_allowed_cidrs
+  internal_lb_allowed_ports = var.internal_lb_allowed_ports
 
-#   # oke public load balancer
-#   public_lb_allowed_cidrs = var.public_lb_allowed_cidrs
-#   public_lb_allowed_ports = var.public_lb_allowed_ports
+  # oke public load balancer
+  public_lb_allowed_cidrs = var.public_lb_allowed_cidrs
+  public_lb_allowed_ports = var.public_lb_allowed_ports
 
-#   # waf integration
-#   enable_waf = var.enable_waf
+  # waf integration
+  enable_waf = var.enable_waf
 
-#   depends_on = [
-#     module.vcn
-#   ]
-# }
+  depends_on = [
+    module.vcn
+  ]
+}
 
 # cluster creation for oke
 module "oke" {
